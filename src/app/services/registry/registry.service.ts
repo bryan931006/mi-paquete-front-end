79bf4fad5afec483567507043add37f099f92915
Michael <michaelgallego14@hotmail.com>
2018-02-09 14:48:28 -0500

    integracion para registrar empresas y usuarios

--- src/app/services/registry/registry.service.ts
+++ src/app/services/registry/registry.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { GLOBAL } from './../global';
 
 @Injectable()
 export class RegistryService {
 
-  constructor() { }
+  public url: string;
+  public identity;
+  public token;
 
+  constructor(private _http: Http) { 
+    this.url = GLOBAL.url;
+  }
+
+  register(user_to_register) {
+    console.log(user_to_register);
+    
+    let params = JSON.stringify(user_to_register);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    return this._http.post(this.url + 'register', params, { headers: headers })
+      .map(res => res.json());
+ 
+  }
 }
